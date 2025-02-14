21a2ee26fd2790c2f4b66b82c8e03d6ccc2fec40
Hasan Yiğit Kaya <hasanyigtkaya@gmail.com>
2025-02-14 15:33:13 +0300

    Update ALLOWED_HOSTS to use specific server IP address
    - Modify ALLOWED_HOSTS to explicitly set server IP
    - Remove dynamic host parsing logic
    - Simplify host configuration for direct server deployment

--- erp_core/settings/production.py
+++ erp_core/settings/production.py
@@ -10,7 +10,7 @@ SECRET_KEY = get_env_variable('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(' ')
+ALLOWED_HOSTS = '68.183.213.111'
 
 # Security settings
 SECURE_SSL_REDIRECT = False  # Temporarily disabled
