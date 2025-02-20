40bcbbc8a5a8b2f4bf9d076321453aeb034adc88
Hasan Yiğit Kaya <hasanyigtkaya@gmail.com>
2025-02-20 18:28:03 +0300

    Add product_code to ProductViewSet filterset fields
    - Expand filterable fields in ProductViewSet to include product_code
    - Maintain existing filtering capabilities for product_type and inventory_category
    - Improve API flexibility by allowing direct filtering on product code

--- erp_core/views/product.py
+++ erp_core/views/product.py
@@ -7,5 +7,9 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]
-    filterset_fields = ['product_type', 'inventory_category']
+    filterset_fields = [
+        'product_code',
+        'product_type',
+        'inventory_category'
+    ]
     search_fields = ['product_code', 'product_name'] 
